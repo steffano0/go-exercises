af0244a826ace2a63fd5ad9f66a02cd754b92a34
steffano0 <steffanouromero@gmail.com>
2023-07-17 13:28:57 -0500

    Add a test for array and slices examples

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -1,9 +1,9 @@
 package main
 
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 
-	for _, v := range numbers {
-		sum += v
+	for _, number := range numbers {
+		sum += number
 	}
 	return
 
